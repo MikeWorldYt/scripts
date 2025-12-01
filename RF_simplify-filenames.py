284e2c0923b832a86e0a1644e7b0f73e71b7badf
mikeworldyt <diazmig98@gmail.com>
2025-12-01 00:31:16 -0300

    fix: Improve filename processing logic

--- RF_simplify-filenames.py
+++ RF_simplify-filenames.py
@@ -1,7 +1,7 @@
 import os, re, random, string
 from collections import defaultdict
 
-COMMON_WORDS = {"image", "photo", "picture", "video", "download", "file", "document", "scan", "sample", "maxresdefault" }
+COMMON_WORDS = {"image", "photo", "picture", "video", "download", "file", "document", "scan", "sample", "maxresdefault", "_n" }
 
 # 🔹 Detectar si una palabra es hexadecimal (mínimo 5 caracteres)
 def es_hexadecimal(palabra):
@@ -17,6 +17,7 @@ def procesar_nombre(base_name):
     for word in COMMON_WORDS:
         if word in base_name.lower():
             filter_name = re.sub(word, "", filter_name, flags=re.IGNORECASE)
+    filter_name = filter_name.replace(".", "-").replace(",", "-")
     partes = re.split(r"([_\-])", filter_name)
     # Caso 1: nombre completo es hexadecimales
     if es_hexadecimal(base_name):
@@ -31,6 +32,8 @@ def procesar_nombre(base_name):
     # Reconstruir nombre
     nuevo_nombre = "".join(nuevas_partes)
     nuevo_nombre = nuevo_nombre.strip("-_")
+    nuevo_nombre = re.sub(r"[ \-_]+", lambda m: m.group(0)[0], nuevo_nombre)
+    nuevo_nombre = re.sub(r"-_|_-", "", nuevo_nombre)
     if not nuevo_nombre or nuevo_nombre in ["-", "_"]:
         return sufijo_hex(4)
     return nuevo_nombre if nuevo_nombre else sufijo_hex(4)
